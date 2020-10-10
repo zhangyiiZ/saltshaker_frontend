d10f317157010b09d055802553a0ff1fb13ce757
ZhangYi <zhangyi2@chinatelecom.cn>
2020-10-10 14:29:16 +0800

    branch

--- src/views/common-components/git-lab_config.vue
+++ src/views/common-components/git-lab_config.vue
@@ -17,6 +17,7 @@
                         <div style="float: right;" >
                             <slot name="create"></slot>
                             <slot name="downMenu"></slot>
+                            <Button type="primary" @click="refresh()">刷新</Button>
                             <Button type="primary" @click="synchronize()">备份</Button>
                             <Modal slot="option" v-model="synchronizeView"  :title="optionTypeName" width="600px">
                                 <Form ref="formSynchronizeValidate" :model="formSynchronizeValidate" :rules="ruleSynchronizeValidate" :label-width="65">
